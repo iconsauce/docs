02d700a5745e9be499f5f04662f7c08caf795603
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-02-14 10:09:22 +0100

    fix: start config

--- scripts/start.js
+++ scripts/start.js
@@ -1,33 +1,37 @@
 const rewire = require('rewire')
 const defaults = rewire('react-scripts/scripts/start.js')
-const config = defaults.__get__('config')
+const webpackConfig = require('react-scripts/config/webpack.config')
 
-config.module.rules.push({
-  test: /iconsauce\.css$/,
-  use: [
-    {
-      loader: 'postcss-loader',
-      options: {
-        postcssOptions: {
-          config: false,
-          ident: 'postcss',
-          plugins: [
-            'postcss-iconsauce',
-            'postcss-flexbugs-fixes',
-            [
-              'postcss-preset-env',
-              {
-                autoprefixer: {
-                  flexbox: 'no-2009',
+defaults.__set__('configFactory', webpackEnv => {
+  const config = webpackConfig(webpackEnv)
+
+  config.module.rules.push({
+    test: /iconsauce\.css$/,
+    use: [
+      {
+        loader: 'postcss-loader',
+        options: {
+          postcssOptions: {
+            config: false,
+            ident: 'postcss',
+            plugins: [
+              'postcss-iconsauce',
+              'postcss-flexbugs-fixes',
+              [
+                'postcss-preset-env',
+                {
+                  autoprefixer: {
+                    flexbox: 'no-2009',
+                  },
+                  stage: 3,
                 },
-                stage: 3,
-              },
+              ],
             ],
-          ],
+          },
         },
       },
-    },
-  ],
-})
+    ],
+  })
 
-module.exports = config
+  return config
+})
