c3c3115ebbb2f7e6c548ccba41f62ea044540598
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-02-21 14:44:24 +0100

    fix: adjust some color

--- src/components/ide/ide.tsx
+++ src/components/ide/ide.tsx
@@ -3,11 +3,41 @@ import clsx from 'clsx'
 import hljs from 'highlight.js/lib/core'
 import xml from 'highlight.js/lib/languages/xml'
 import css from 'highlight.js/lib/languages/css'
-import 'highlight.js/styles/an-old-hope.css'
-// import 'highlight.js/styles/atom-one-dark.css'
+// import 'highlight.js/styles/an-old-hope.css'
+import 'highlight.js/styles/atom-one-dark.css'
 hljs.registerLanguage('xml', xml)
 hljs.registerLanguage('css', css)
 import { ComponentProps } from '../../meta/component'
+
+// const START_DELAY = 500
+// const CHAR_DELAY = 50
+// const SAVE_DELAY = 50
+// const BUILD_DELAY = 100
+// const BLOCK_DELAY = 1000
+
+// const htmlCode = [
+//   '<cmp-li class="mdi/lock">Material Design Icons</cmp-li>',
+//   '<cmp-li class="fa/solid/car">FontAwesome</cmp-li>',
+//   '<cmp-li class="my/iconsauce">Custom Icon</cmp-li>',
+// ]
+
+// const cssCode1 = [
+//   '.mdi\/lock::before { content: "\2f" }',
+//   '.fa\/solid\/car::before { content: "\2f" }',
+//   '.my\/iconsauce::before { content: "\2f" }',
+// ]
+
+// const cssCode2 = [
+//   '[class^="mdi/"], [class*=" mdi/"]',
+//   '[class^="mdi/"], [class*=" mdi/"], [class^="fa/"], [class*=" fa/"]',
+//   '[class^="mdi/"], [class*=" mdi/"], [class^="fa/"], [class*=" fa/"], [class^="my/"], [class*=" my/"]',
+// ]
+
+// const fontCode = [
+//   'A...+45...Z',
+//   'A...+167...Z',
+//   'A...+315...Z',
+// ]
 
 interface IdeProps {
   children?: ReactNode|ReactNode[],
@@ -64,8 +94,8 @@ const Code = ({
   code,
   language,
 }: CodeProps): ReactElement =>
-  <div className={clsx('grow flex text-mono-code bg-adjust-tone-01 p-4 text-adjust-tone-0 overflow-hidden', className)}>
-    <pre className={clsx(className, 'm-0 text-mono-hack text-adjust-tone-05 box-border')} dangerouslySetInnerHTML={{ __html: hljs.highlight(code.trim(), { language }).value }} />
+  <div className={clsx('grow flex text-mono-code bg-adjust-tone-01 p-4 text-adjust-tone-06 overflow-hidden', className)}>
+    <pre className={clsx(className, 'm-0 text-mono-hack box-border')} dangerouslySetInnerHTML={{ __html: hljs.highlight(code.trim(), { language }).value }} />
   </div>
 
 const Ide = ({
@@ -75,21 +105,25 @@ const Ide = ({
     <div className='grid gap-2 wide:grid-cols-2'>
       <Column>
         <File>
-          <Tab icon='mdi/language-html5 text-label-orange-06' className='text-label-orange-08' status={'unsaved'}>
+          <Tab icon='mdi/language-html5 text-label-orange-06' className='text-adjust-tone-06' status={'unsaved'}>
             page.html
           </Tab>
           <Code language="html" code={`
-<nav>
-  <cmp-button class="mdi/lock">Login</cmp-button>
-  <cmp-button class="fa/solid/car">Travel</cmp-button>
-  <cmp-button class="mdi/twitter">Share</cmp-button>
-</nav>
+<ul>
+  <cmp-li class="mdi/lock">Material Design Icons</cmp-li>
+  <cmp-li class="fa/solid/car">FontAwesome</cmp-li>
+  <cmp-li class="mi/twitter">Custom Icon</cmp-li>
+</ul>
+<p>
+  Get all the SVG icons you need to a font icons CSS,
+  from different node modules or directly inside your project.
+</p>
             `}/>
         </File>
       </Column>
       <Column>
         <File>
-          <Tab icon='mdi/language-css3 text-label-sky-06' className='text-label-sky-08'>
+          <Tab icon='mdi/language-css3 text-label-sky-06' className='text-adjust-tone-06'>
             build.css
           </Tab>
           <Code language="css" code={String.raw`
