6bba645f1a86419d27c2448a8805b512264123d5
Vittorio Vittori <vittorio.vittori@maggioli.it>
2022-03-01 18:27:21 +0100

    docs: update infos

--- scripts/build.js
+++ scripts/build.js
@@ -3,26 +3,14 @@ const defaults = rewire('react-scripts/scripts/build.js')
 const config = defaults.__get__('config')
 
 config.module.rules.push({
-  test: /iconsauce\.css$/,
+  test: /\.css$/,
   use: [
     {
       loader: 'postcss-loader',
       options: {
         postcssOptions: {
-          config: false,
-          ident: 'postcss',
           plugins: [
             'postcss-iconsauce',
-            'postcss-flexbugs-fixes',
-            [
-              'postcss-preset-env',
-              {
-                autoprefixer: {
-                  flexbox: 'no-2009',
-                },
-                stage: 3,
-              },
-            ],
           ],
         },
       },
